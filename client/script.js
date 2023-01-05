f073c83e7d4ffc09165924d33f48243c334b2833
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2023-01-05 15:58:47 +0200

    update the app

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
   //Fetch data from the server
   //bot response
-  const response = await fetch("https://codeai-wz02.onrender.com/", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
